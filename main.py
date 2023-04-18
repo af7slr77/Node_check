4821ee1b8c3e08f2a30eeb4a5320506e6efd7a37
Andrey <af7slr722@gmail.com>
2023-04-18 21:05:23 +0000

    add models.py, funcs: call_url and main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import asyncio
+import aiohttp
 import json
-
+from models import write_to_db
 
 
 
@@ -18,12 +20,12 @@ def get_nodes_urls(api_url):
 	nodes_data = res['result']['ssnlist']
 	for key in nodes_data:
 		url = nodes_data[f'{key}']['arguments'][5]
-		name = nodes_data[f'{key}']['arguments'][3]
+		#name = nodes_data[f'{key}']['arguments'][3]
+		#urls.append({'node_url':url, 'name':name})
 		urls.append(url)
 	return urls
 
-
-def get_nodes_info(urls):
+async def call_url(url):
 	params = json.dumps( {
 		"id": "1",
 		"jsonrpc": "2.0",
@@ -31,12 +33,33 @@ def get_nodes_info(urls):
 		"params": [""]
 	} )
 	headers = {"Content-Type": "application/json"}
-	res = requests.post(urls[1], data=params)
-	return res
 
-def  main():
+	async with aiohttp.ClientSession(trust_env=True) as session:
+		try:
+			async with session.post(url, data=params, headers=headers,) as response:
+				async with session.post(url, data=params, headers=headers, timeout=3) as response:
+					resp = await response.json()
+					current_dse_poch = resp['result']['CurrentDSEpoch']
+					current_mini_epoch = resp['result']['CurrentMiniEpoch']
+					return current_dse_poch, current_mini_epoch
+		except Exception as ex:
+			current_dse_poch = 'error'
+			current_mini_epoch = 'error'
+			return current_dse_poch, current_mini_epoch
+
+
+async def main():
 	api_url = 'https://api.zilliqa.com/'
-	print(get_nodes_info(get_nodes_urls(api_url)).json()['result'])
+	urls = get_nodes_urls(api_url)
+	while True:
+		tasks = []
+		for url in urls:
+			tasks.append(asyncio.create_task(call_url(url)))
+			if len(tasks) == len(urls):
+				res = await asyncio.gather(*tasks)
+				print(res)
+				tasks = []
 
 if __name__ == '__main__':
-	main()
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(main())
--- models.py
+++ models.py
@@ -2,16 +2,25 @@ import sqlalchemy as db
 
 
 
-engine = db.create_engine("sqlite://test.db")
+engine = db.create_engine("sqlite:///db.test")
 connection = engine.connect()
 metadata = db.MetaData()
-node = db.Table(Nodes, metadata,
-    db.Column(id, db.Integer, primary_key=True),
-    db.Column(id, db.String,primary_key=True),
-    db.Column(id, db.Integer, primary_key=True),
-    db.Column(id, db.Integer, primary_key=True),
-    db.Column(id, db.Integer, primary_key=True),
-    db.Column(id, db.Integer, primary_key=True)
-
-
+nodes = db.Table('nodes', metadata,
+	db.Column('id', db.String, primary_key=True),
+	db.Column('node_url', db.String),
+	db.Column('node_name', db.String),
+	db.Column('CurrentDSEpoch', db.String),
+	db.Column('CurrentMiniEpoch', db.String),
 )
+
+metadata.create_all(engine)
+
+def write_to_db(node_url, node_name, node_query):
+	insertian_query = nodes.insert().values([
+		{
+			'node_url': node_url,
+			'node_name': node_name,
+			'CurrentDSEpoch': node_query['CurrentDSEpoch'],
+			'CurrentMiniEpoch': node_query['CurrentMiniEpoch'],
+		}
+	])
