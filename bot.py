6b86a9ca4fe010a68cfa324a9d9bee5603a2a582
Andrey <af7slr722@gmail.com>
2023-04-21 21:16:14 +0000

    change func 'add node_tracking'

--- bot.py
+++ bot.py
@@ -9,24 +9,27 @@ dp = Dispatcher(bot)
 messages_id_data = [] # store messages id
 
 
-@dp.message_handler()
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):
 	user_id = message.from_user.id
+	node_for_tracking = message.text
 	while True:
-		await check_nodes(user_id)
+		await check_nodes(user_id, node_for_tracking)
 		# await clear_chat() 
 	
 
 
-async def check_nodes(user_id):
+async def check_nodes(user_id, node_for_tracking):
 	responce = await get_nodes_info()
 	print(responce)
 	for i in responce:
-		if i['current_dse_poch'] == 'error' or i['current_mini_epoch'] == 'error':
-			msg = i['name'] + ' ' + 'something went wrong, check the node!'
-			send_message = await bot.send_message(user_id, msg)
-			message_id = send_message['message_id']
-			messages_id_data.append(message_id)
+		if i['name'] == node_for_tracking:
+			print(node_for_tracking)
+			if i['current_dse_poch'] == 'error' or i['current_mini_epoch'] == 'error':
+				msg = i['name'] + ' ' + 'something went wrong, check the node!'
+				send_message = await bot.send_message(user_id, msg)
+				message_id = send_message['message_id']
+				messages_id_data.append(message_id)
 
 
 
@@ -35,15 +38,6 @@ async def clear_chat(): # Deletes all messages from the chat except the last 6
 		for id in messages_id_data[:-30]:
 			await bot.delete_message(chat_id, id)
 			messages_id_data.remove(id)
-
-
-
-# async def main():
-
-# 	while True:
-# 		await check_nodes()
-# 		await clear_chat() 
-# 		# await asyncio.sleep(225)
 
 
 if __name__ == '__main__':
