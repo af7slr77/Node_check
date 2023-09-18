c4dd4e2f143f0bd6ae5d94876ec711d54f904ef2
Andrey <af7slr722@gmail.com>
2023-09-18 19:36:58 +0500

    refactoring /logs, midelwares/register_check.py modules

--- logs/logs.py
+++ logs/logs.py
@@ -52,3 +52,20 @@ def init_bot_logger(name):
     fh.setLevel(logging.DEBUG)
     logger.addHandler(sh)
     logger.addHandler(fh)
+
+def init_register_check_logger(name):
+    logger = logging.getLogger(name)
+    FORMAT = '%(asctime)s - %(name)s :: %(line)s - %(levelname)s - %(message)s'
+    logger.setLevel(logging.DEBUG)
+    sh = logging.StreamHandler()
+    sh.setFormatter(logging.Formatter(FORMAT))
+    sh.setLevel(logging.DEBUG)
+    fh = RotatingFileHandler(
+        filename="/tmp/register_check.log", 
+        maxBytes=5, 
+        backupCount=2
+    ) 
+    fh.setFormatter(logging.Formatter(FORMAT))
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(sh)
+    logger.addHandler(fh)
--- midelwares/register_check.py
+++ midelwares/register_check.py
@@ -1,22 +1,32 @@
-import asyncio
 from typing import Callable, Dict, Any, Awaitable, Union
 from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
-from models.models import Node, User
+from models.models import User
 from sqlalchemy import select
-from sqlalchemy.orm import lazyload, joinedload
 from datetime import datetime
+import logging
+from logs.logs import init_register_check_logger
+
+init_register_check_logger("register_check")
+register_check_logger = logging.getLogger('register_check')
+
 
 class RegisterCheck(BaseMiddleware):
 	async def __call__(
 		self,
-		handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+		handler: Callable[
+			[Message, Dict[str, Any]], 
+			Awaitable[Any]
+		],
 		event: Union[Message, CallbackQuery],
 		data: Dict[str, Any] 
 	) -> Any:
 		async_session: async_session = data['async_session']
 		async with async_session() as session:
-			result = await session.execute(select(User).where(User.user_telegram_id == event.from_user.id))
+			stmt = select(User).where(
+				User.user_telegram_id == event.from_user.id
+			)
+			result = await session.execute(stmt)
 			user = result.one_or_none()
 			if user is not None:
 				pass
@@ -30,5 +40,5 @@ class RegisterCheck(BaseMiddleware):
 					session.add(new_user)
 					await session.commit()
 				except Exception as ex:
-					print('RegisterCheck',  ex)
+					blocks_logger.debug(ex, extra={'line':42})
 		return await handler(event, data)
