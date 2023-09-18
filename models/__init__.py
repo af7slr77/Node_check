b48274c29d1e5ecbf8a53d41a6d5d49647e05194
Andrey <af7slr722@gmail.com>
2023-09-18 20:14:10 +0500

    refactoring /models modules

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .models import User, Node, NodesUsers, Records
-# from .block import Blocks
-# from models.users_nodes import *
-# __all__ = ['Users_nodes']
--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, VARCHAR, Table, BIGINT
-from sqlalchemy.orm import relationship, lazyload
-from datetime import datetime
+from sqlalchemy import Column, Integer
+from sqlalchemy import String, ForeignKey, VARCHAR
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from config.zilliqa import TRUST_COEFFICIENT
 
@@ -9,8 +9,14 @@ BaseModel = declarative_base()
 class NodesUsers(BaseModel):
 	__tablename__ = "nodes_users"
 
-	node_id = Column(Integer, ForeignKey("nodes.node_id"), primary_key=True, nullable=False)
-	user_id = Column(Integer, ForeignKey("users.user_id"), primary_key=True, nullable=False)
+	node_id = Column(
+		Integer, ForeignKey("nodes.node_id"), 
+		primary_key=True, nullable=False
+	)
+	user_id = Column(
+		Integer, ForeignKey("users.user_id"), 
+		primary_key=True, nullable=False
+	)
 
 	def __repr__(self):
 		return f'{self.user_id, self.node_id}'
@@ -19,23 +25,51 @@ class NodesUsers(BaseModel):
 class Node(BaseModel):
 	__tablename__ = 'nodes'
 
-	node_id = Column(Integer, unique=True,  primary_key=True, nullable=False)
-	node_url = Column(String, unique=True, nullable=False)
+	node_id = Column(
+		Integer, unique=True,  
+		primary_key=True, nullable=False
+	)
+	node_url = Column(
+		String, unique=True, 
+		nullable=False
+	)
 	node_name = Column(String, nullable=False)
-	nodes_users = relationship("User", secondary='nodes_users', backref='nodes', lazy='selectin', )
-	records = relationship('Records', backref='nodes', lazy='selectin')
-	trust_coefficient = Column(Integer, default=TRUST_COEFFICIENT, nullable=False)
+	nodes_users = relationship(
+		"User", secondary='nodes_users', 
+		backref='nodes', lazy='selectin'
+	)
+	records = relationship(
+		'Records', backref='nodes',
+		 lazy='selectin'
+	)
+	trust_coefficient = Column(
+		Integer, default=TRUST_COEFFICIENT, 
+		nullable=False
+	)
 	def __repr__(self):
 		return f'{self.node_id, self.node_url, self.node_name}'
 
 
 class User(BaseModel):
 	__tablename__ = 'users'
-	user_id = Column(Integer, unique=True, nullable=False,  primary_key=True)
-	user_telegram_id = Column(Integer, unique=True, nullable=False)
-	username = Column(VARCHAR(32), unique=True, nullable=False)
+	user_id = Column(
+		Integer, unique=True, 
+		nullable=False,  primary_key=True
+	)
+	user_telegram_id = Column(
+		Integer, unique=True, 
+		nullable=False
+	)
+	username = Column(
+		VARCHAR(32), unique=True, 
+		nullable=False
+	)
 	reg_date = Column(Integer, default=50, nullable=False)
-	users_nodes = relationship('Node', secondary='nodes_users', backref='users', lazy='selectin', viewonly=True )
+	users_nodes = relationship(
+		'Node', secondary='nodes_users', 
+		backref='users', lazy='selectin', 
+		viewonly=True
+	)
 
 	def __repr__(self):
 		return f'{self.user_id, self.user_telegram_id, self.username, self.reg_date}'
@@ -43,7 +77,10 @@ class User(BaseModel):
 class Records(BaseModel):
 	__tablename__ = 'records'
 
-	record_id = Column(Integer, unique=True,  primary_key=True)
+	record_id = Column(
+		Integer, unique=True,  
+		primary_key=True
+	)
 	update_time = Column(Integer, nullable=False)
 	node_id = Column(Integer, ForeignKey('nodes.node_id'))
 	current_ds_epoch = Column(Integer)
