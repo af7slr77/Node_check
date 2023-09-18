29b3ec8a2f69690b8f9c4549e9541dc1127519ee
Andrey <af7slr722@gmail.com>
2023-09-18 14:19:07 +0500

    refactoring the cancel_subscription.py module

--- callbacks/cancel_subscription.py
+++ callbacks/cancel_subscription.py
@@ -4,7 +4,10 @@ from worker.worker_db import Worker
 from .callbacks import CancelSubscriptionCallback
 
 
-async def cancel_subscription(call: types.CallbackQuery, callback_data: CancelSubscriptionCallback):
+async def cancel_subscription(
+	call: types.CallbackQuery, 
+	callback_data: CancelSubscriptionCallback
+	):
 	async_session = await get_async_session()
 	node_name = callback_data.node_name
 	tg_user_id = call.from_user.id
