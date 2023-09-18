79fbf69ecbf80656167b9afde7e1bc9790f62265
Andrey <af7slr722@gmail.com>
2023-09-18 15:33:58 +0500

    refactoring the subscribe.py module

--- callbacks/subscribe.py
+++ callbacks/subscribe.py
@@ -3,7 +3,11 @@ from db.engine import get_async_session
 from worker.worker_db import Worker
 from .callbacks import SubscribeCallback
 
-async def subscribe(call: types.CallbackQuery, callback_data: SubscribeCallback):
+
+async def subscribe(
+	call: types.CallbackQuery, 
+	callback_data: SubscribeCallback
+	):
 	async_session = await get_async_session()
 	node_name = callback_data.node_name
 	tg_user_id = call.from_user.id
@@ -12,6 +16,9 @@ async def subscribe(call: types.CallbackQuery, callback_data: SubscribeCallback)
 		msg = f"You are already subscribed to {node_name}!"
 		await call.message.answer(text=msg)
 	else:
-		make_subscribe = await worker._subscribe(node_name, tg_user_id)
+		make_subscribe = await worker._subscribe(
+			node_name, 
+			tg_user_id
+		)
 		msg = f"You are subscribed to {node_name}!"
 		await call.message.answer(text=msg)
