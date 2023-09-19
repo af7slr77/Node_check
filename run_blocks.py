d533721095d411291c5bd458202a0c2ee4a1ea19
Andrey <af7slr722@gmail.com>
2023-09-19 22:49:28 +0500

    refactoring run_bot module

--- run_blocks.py
+++ run_blocks.py
@@ -7,6 +7,7 @@ from logs.logs import init_worker_logger
 init_worker_logger('worker')
 run_worker_logger = logging.getLogger('worker.run_worker')
 
+
 def main():
 	async_session = asyncio.run(get_async_session())
 	block = BlocksWorker(async_session=async_session)
--- run_bot.py
+++ run_bot.py
@@ -11,15 +11,19 @@ from logs.logs import init_bot_logger
 init_bot_logger('bot')
 blocks_logger = logging.getLogger('bot.run_bot')
 
+
 async def bot():
 	async_session = await get_async_session()
-	#Gen commands menu
 	comands_for_bot = []
 	for cmd in bot_commands:
-		comands_for_bot.append(BotCommand(command=cmd[0], description=cmd[1]))
+		comands_for_bot.append(
+			BotCommand(
+				command=cmd[0], 
+				description=cmd[1]
+			)
+		)
 	dp = Dispatcher()
 	bot = Bot(token=os.getenv('NODE_TOKEN'))
-	#Set commands
 	await bot.set_my_commands(commands=comands_for_bot)
 	await register_user_comands(dp)
 	await dp.start_polling(bot, async_session=async_session)
@@ -28,7 +32,4 @@ if __name__ == '__main__':
 	try:
 		asyncio.run(bot())
 	except Exception as ex:
-		line = {
-			'line':31
-				}
-		blocks_logger.warning(msg=ex, extra=line)
+		blocks_logger.warning(msg=ex, extra={'line':31})
