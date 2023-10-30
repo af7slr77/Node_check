dee60c00052b3e7cf299f373ff12f9381c2dff74
af7slr77 <af7slr722@gmail.com>
2023-10-30 17:09:26 +0500

    add annotations to callbacks/subscribe.py

--- callbacks/subscribe.py
+++ callbacks/subscribe.py
@@ -7,18 +7,19 @@ from .callbacks import SubscribeCallback
 async def subscribe(
 	call: types.CallbackQuery, 
 	callback_data: SubscribeCallback
-	):
+	) -> None:
 	async_session = await get_async_session()
-	node_name = callback_data.node_name
-	tg_user_id = call.from_user.id
+	node_name: str = callback_data.node_name
+	tg_user_id: int = call.from_user.id
 	worker = Worker(async_session)
-	if await worker._check_subscribe(node_name, tg_user_id):
-		msg = f"You are already subscribed to {node_name}!"
-		await call.message.answer(text=msg)
-	else:
-		make_subscribe = await worker._subscribe(
-			node_name, 
-			tg_user_id
-		)
-		msg = f"You are subscribed to {node_name}!"
-		await call.message.answer(text=msg)
+	if call.message:
+		if await worker._check_subscribe(node_name, tg_user_id):
+			subscribed_msg: str = f"You are already subscribed to {node_name}!"
+			await call.message.answer(text=subscribed_msg)
+		else:
+			make_subscribe = await worker._subscribe(
+				node_name, 
+				tg_user_id
+			)
+			already_subscribed_msg: str = f"You are subscribed to {node_name}!"
+			await call.message.answer(text=make_subscribe)
