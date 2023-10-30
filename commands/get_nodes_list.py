0b69ea4b6303413b52bd3cd7c0a9cbdf9d8bf0e5
af7slr77 <af7slr722@gmail.com>
2023-10-30 18:27:35 +0500

    add annotations to commands/get_nodes_lsit.py

--- commands/get_nodes_list.py
+++ commands/get_nodes_list.py
@@ -1,13 +1,13 @@
 from worker.worker_db import Worker
 from db.engine import get_async_session
+from typing import List, Tuple
 
-
-async def get_nodes_list():
+async def get_nodes_list() -> List[str]:
 	async_session = await get_async_session()
 	worker = Worker(async_session)
-	data = await worker._buttons()
-	new_nodes_list = []
+	data: List[Tuple] = await worker._buttons()
+	new_nodes_list: List = []
 	for node in data:
-		node_name = node[0].node_name
+		node_name: str = node[0].node_name
 		new_nodes_list.append(node_name)
 	return new_nodes_list
