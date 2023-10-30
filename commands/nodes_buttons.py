c95c84a01c9c60b193cb1af144b8c1ba5a5fb147
af7slr77 <af7slr722@gmail.com>
2023-10-30 18:55:30 +0500

    add annotations to commands/nodes_buttons.py

--- commands/nodes_buttons.py
+++ commands/nodes_buttons.py
@@ -2,17 +2,18 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.keyboard import InlineKeyboardButton
 from aiogram import types
 from .get_nodes_list import get_nodes_list
+from typing import List
 
 
-async def get_sorted_nodes_list():
-	nodes_list = await get_nodes_list()
-	sorted_nodes_list = [
+async def get_sorted_nodes_list() -> List[List[str]]:
+	nodes_list: List[str] = await get_nodes_list()
+	sorted_nodes_list: List[List[str]] = [
 		nodes_list[elem:elem+2] 
 		for elem in range(0, len(nodes_list), 2)]
 	return sorted_nodes_list
 
-async def get_nodes_buttons(message:types.Message):
-	sorted_nodes_list = await get_sorted_nodes_list()
+async def get_nodes_buttons(message:types.Message) -> None:
+	sorted_nodes_list: List[List[str]] = await get_sorted_nodes_list()
 	inline_kb = InlineKeyboardBuilder()
 	for nodes in sorted_nodes_list:
 		if len(nodes) == 1:
@@ -33,7 +34,7 @@ async def get_nodes_buttons(message:types.Message):
 					callback_data=f"{nodes[1]}"
 				)
 			)
-	msg = 'Select a node to get information:'
+	msg: str = 'Select a node to get information:'
 	await message.answer(
 		text=msg, 
 		reply_markup=inline_kb.as_markup()
