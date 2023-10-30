867d3d2fbf0284ab7545a9bdc24390431afbaf62
af7slr77 <af7slr722@gmail.com>
2023-10-30 18:42:05 +0500

    add annotations to commands/help.py

--- commands/help.py
+++ commands/help.py
@@ -6,16 +6,17 @@ from commands.bot_commands import bot_commands
 async def help_comand(
 	message: types.Message,
 	command: CommandObject
-	):
+	) -> None:
 	if command.args:
 		for cmd in bot_commands:
-			if cmd[0] == command.args:
-				return await message.answer(f"{cmd[0]} - {cmd[2]}")
+			if cmd[0] == command.args[0]:
+				await message.answer(f"{cmd[0]} - {cmd[2]}")
 		else:
-			return await message.answer(text="Comand not found")
+			command_not_found_msg: str = "Command not found"
+			await message.answer(text=command_not_found_msg)
 	else:
-		return await help_func(message)
+		await help_func(message)
 	
 async def help_func(message:types.Message):
-	msg = "To get a list of available nodes, select the command: /get_nodes <command>"
-	return await message.answer(text=msg)
+	msg: str = "To get a list of available nodes, select the command: /get_nodes <command>"
+	await message.answer(text=msg)
