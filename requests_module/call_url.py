70dec319af259f2da7182b885edd6665ecfc6def
af7slr77 <af7slr722@gmail.com>
2023-10-31 11:45:28 +0500

    add annotations to requests_module/call_url.py

--- requests_module/call_url.py
+++ requests_module/call_url.py
@@ -10,7 +10,7 @@ init_worker_logger('request_modules')
 logger = logging.getLogger('request_modules.call_url')
  
 
-def call_url(node_url: str) -> Dict[str, Union[float, None]]:
+def call_url(node_url: str) -> Dict[str, Union[int, None, float]]:
 	params: str = json.dumps(
 		{
 			"id": "1",
@@ -22,17 +22,17 @@ def call_url(node_url: str) -> Dict[str, Union[float, None]]:
 	headers = {"Content-Type": "application/json"}
 	proxies = {''}
 	try:
-		start_time = time()
+		start_time: float = time()
 		resp = requests.post(
 			node_url, 
 			data=params, 
 			headers=headers, 
 			timeout = MAX_NODE_TIMEOUT_SECOND
 		).json()
-		end_time = time()
-		response_time = end_time - start_time
-		current_ds_epoch = int(resp['result']['CurrentDSEpoch'])
-		current_mini_epoch = int(resp['result']['CurrentMiniEpoch'])
+		end_time: float = time()
+		response_time: float = end_time - start_time
+		current_ds_epoch: int = int(resp['result']['CurrentDSEpoch'])
+		current_mini_epoch: int = int(resp['result']['CurrentMiniEpoch'])
 		if response_time < MAX_NODE_TIMEOUT_SECOND:
 			return {
 				'status': 200,
